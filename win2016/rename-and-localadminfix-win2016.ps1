11348306cb7e561a00a8e538116ba72bf5ff54d4
Jeff Hibberd <jeff@jeffhibberd.com>
2018-09-11 15:11:09 +0100

    vars from args and fixed newline

--- win2016/rename-and-localadminfix-win2016.ps1
+++ win2016/rename-and-localadminfix-win2016.ps1
@@ -1,16 +1,19 @@
-# set variables here like:
-# $newComputerName = 'newname'
-# $newNVDomain = 'lan'
+# Get vars from script args
+$newComputerName = $args[0]
+$newNVDomain = $args[1]
 
 if ( ($newComputerName -eq $null) -or ($newNVDomain -eq $null) ){
-	read-host -Prompt "Please read script comments and set variables in it first!"
+	write-host "Syntax: "$PSCommandPath" <new computer name> <new primary dns suffix"
 	exit
+}
+Else {
+	write-host "rename this computer to "$newComputerName"."$newNVDomain"?" 
+	read-host -Prompt "Press Enter to continue or CTRL-C to exit"
 }
 
 # rename and set fqdn
 Rename-Computer -NewName $newComputerName
-Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters -Name "NV Domain" -Value
-$newNVDomain
+Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters -Name "NV Domain" -Value $newNVDomain
 Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters -Name SyncDomainWithMembership -Value 0
 
 # registry hack to enable local admins to configure winrm (requires restart)
