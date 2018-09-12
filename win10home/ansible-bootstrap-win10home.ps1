1c93e70ddcb01f088c272fae1c47fdffd1ec910d
Jeff Hibberd <jeff@jeffhibberd.com>
2018-09-12 08:38:10 +0100

    updated win10home script to use code from 2016

--- win10home/ansible-bootstrap-win10home.ps1
+++ win10home/ansible-bootstrap-win10home.ps1
@@ -20,6 +20,14 @@
 
 $ErrorActionPreference = "Stop"
 
+#Vars
+$logFile = 'C:\log\winrmscript.log'
+$ansibleUserName = 'ansible'
+$fqdn=[System.Net.Dns]::GetHostByName($env:computerName).HostName
+Write-Host "My FQDN appears to be "$fqdn" Press ENTER to continue with this FQDN"
+Write-Host "There is a rename computer script around here somewhere ;)"
+Read-Host
+
 #check windows version
 $desiredbuild = 17134
 $detectedbuild = [System.Environment]::OSVersion.Version.Build
@@ -32,20 +40,59 @@ if ($detectedbuild -ne $desiredbuild) {
 } Else {
 Write-host "Build version seems to be in order, here goes..."
 }
+
+#Check if we've been here before
+if (Test-Path $logFile) {
+        write-host "Found a log File at "$logFile" - cowardly refusing to run script twice."
+        write-host "Delete "$logFile" if you want to try again"
+        Read-Host -Prompt "Press Enter to exit or ctrl-C to return to the shell"
+        Exit
+} Else {
+        Write-Host "no logfile found, continuing"
+        New-Item $logFile -Itemtype file -Force
+}
+
+#Check that we can adminster winrm (permissions may be wrong)
+try {
+  winrm enumerate winrm/config/Listener
+  "I can enumerate winrm OK" | Out-File $logFile -Append
+}
+Catch {
+  Read-Host -Prompt "Error: Can't enumerate winrm properly. check the rename script for clues"
+  Exit
+}
+
+#ansible user - remove first (silently continue if not there)
+remove-localuser -Name $ansibleUserName -ErrorAction SilentlyContinue
+try {
+  $password = Read-Host -AsSecureString -Prompt "ansible user password:"
+  New-localuser -Name $ansibleUserName -Password $password -ErrorAction Stop
+  "Added ansible user" | Out-File $logFile -Append
+  Add-LocalGroupMember -Group Administrators -Member ansible -ErrorAction Stop
+  "Added ansible user to Administrators group" | Out-File $logFile -Append
+}
+Catch {
+  "Error adding ansible user" | Out-File $logFile -Append
+}
+
 # do initial winrm quickconfig (HTTP only)
 winrm quickconfig -quiet
 
-# create a self signed cert for the system hostname and use its thumbprint to add the listener.
-$fqdn=[System.Net.Dns]::GetHostByName($env:computerName).HostName
+# remove HTTP listener and create an HTTPS one
+Get-ChildItem -Path WSMan:\localhost\Listener | Where-Object { $_.Keys -contains "Transport=HTTP" } | Remove-Item -Recurse -Force
 $certificateforwinrm=New-SelfSignedCertificate -DnsName $fqdn -CertStoreLocation Cert:\LocalMachine\My
 $mycommand = 'winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname="'+$fqdn+'"; CertificateThumbprint="'+$certificateforwinrm.Thumbprint+'"}'
-cmd /c $mycommand
-
-# Remove the initial HTTP listener
-Get-ChildItem -Path WSMan:\localhost\Listener | Where-Object { $_.Keys -contains "Transport=HTTP" } | Remove-Item -Recurse -Force
+if (cmd /c $mycommand) {
+  "Created winrm HTTPS listener" | Out-File $logFile -Append
+}
+Else {
+  "Error creating HTTPS listener. HINT is winrm accessible to local admins? The computer rename script might help" | Out-File $logFile -Append
+  Write-Host "ERROR: see "$logFile" for details"
+}
 
 # remove its firewall rule
 Remove-NetFirewallRule -DisplayName "Windows Remote Management (HTTP-In)"
 
 # allow TCP port 5986 thru firewall.
+set-netconnectionprofile -InterfaceAlias Ethernet -NetworkCategory Private
 New-NetFirewallRule -DisplayName 'WinRM HTTPS' -Profile @('Domain', 'Private') -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('5986')
