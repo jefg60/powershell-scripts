97150b2aba979b8ea4199cf526f772dd5854d644
Jeff Hibberd <jeff@jeffhibberd.com>
2019-02-01 17:25:13 +0000

    update win2016 bootstrap script

--- win2016/ansible-bootstrap-win2016.ps1
+++ win2016/ansible-bootstrap-win2016.ps1
@@ -1,7 +1,7 @@
-# Script to configure winrm with self-signed HTTPS certificate.
-# Useful for configuring windows with ansible.
-# Tested on a non-domain windows 2016 box
-# May not work on a partially configured machine. USE WITH CAUTION.
+# Simple script to configure winrm with self-signed HTTPS certificate.
+# Useful for configuring windows with ansible. No error checking yet, so
+# may not work on a partially configured machine. USE WITH CAUTION.
+# Tested on a windows 10 home box.
 #
 # Copyright (C) 2018 Jeff Hibberd
 #    This program is free software: you can redistribute it and/or modify
@@ -17,13 +17,16 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+#
 
 $ErrorActionPreference = "Stop"
 
-# windows Build version the script was written for
-$desiredBuild = 14393
+function datestring {
+  param($Message)
+  (Get-Date).ToString("yyyy/M/dd hh:mm:ss") + " $Message"
+}
 
-# other vars and FQDN check
+#Vars
 $logFile = 'C:\log\winrmscript.log'
 $ansibleUserName = 'ansible'
 $fqdn=[System.Net.Dns]::GetHostByName($env:computerName).HostName
@@ -32,32 +35,33 @@ Write-Host "There is a rename computer script around here somewhere ;)"
 Read-Host
 
 #check windows version
-$detectedBuild = [System.Environment]::OSVersion.Version.Build
-if ($detectedBuild -ne $desiredBuild) {
-	write-host "This script is for windows Build "$desiredBuild
-	write-host "It appears to be running on Build "$detectedBuild
-	write-host "Please try a different script"
-	Read-Host -Prompt "Press Enter to exit or ctrl-C to return to the shell"
-	Exit
+$desiredbuild = 14393
+$detectedbuild = [System.Environment]::OSVersion.Version.Build
+if ($detectedbuild -ne $desiredbuild) {
+        write-host "This script is for windows build "$desiredbuild
+        write-host "It appears to be running on build "$detectedbuild
+        write-host "Please try a different script"
+        Read-Host -Prompt "Press Enter to exit or ctrl-C to return to the shell"
+        Exit
 } Else {
-	Write-host "Build version seems to be in order, here goes..."
+Write-host "Build version seems to be in order, here goes..."
 }
 
 #Check if we've been here before
 if (Test-Path $logFile) {
-	write-host "Found a log File at "$logFile" - cowardly refusing to run script twice."
-	write-host "Delete "$logFile" if you want to try again"
-	Read-Host -Prompt "Press Enter to exit or ctrl-C to return to the shell"
-	Exit
+        write-host "Found a log File at "$logFile" - cowardly refusing to run script twice."
+        write-host "Delete "$logFile" if you want to try again"
+        Read-Host -Prompt "Press Enter to exit or ctrl-C to return to the shell"
+        Exit
 } Else {
-	Write-Host "no logfile found, continuing"
-	New-Item $logFile -Itemtype file -Force
+        Write-Host "no logfile found, continuing"
+        New-Item $logFile -Itemtype file -Force
 }
 
 #Check that we can adminster winrm (permissions may be wrong)
 try {
   winrm enumerate winrm/config/Listener
-  "I can enumerate winrm OK" | Out-File $logFile -Append
+  datestring -Message "I can enumerate winrm OK" | Out-File $logFile -Encoding ascii -Append
 }
 Catch {
   Read-Host -Prompt "Error: Can't enumerate winrm properly. check the rename script for clues"
@@ -69,28 +73,32 @@ remove-localuser -Name $ansibleUserName -ErrorAction SilentlyContinue
 try {
   $password = Read-Host -AsSecureString -Prompt "ansible user password:"
   New-localuser -Name $ansibleUserName -Password $password -ErrorAction Stop
-  "Added ansible user" | Out-File $logFile -Append
+  datestring -Message "Added ansible user" | Out-File $logFile -Encoding ascii -Append
   Add-LocalGroupMember -Group Administrators -Member ansible -ErrorAction Stop
-  "Added ansible user to Administrators group" | Out-File $logFile -Append
+  datestring -Message "Added ansible user to Administrators group" | Out-File $logFile -Encoding ascii -Append
 }
 Catch {
-  "Error adding ansible user" | Out-File $logFile -Append
+  datestring -Message "Error adding ansible user" | Out-File $logFile -Encoding ascii -Append
 }
+
+# do initial winrm quickconfig (HTTP only)
+winrm quickconfig -quiet
 
 # remove HTTP listener and create an HTTPS one
 Get-ChildItem -Path WSMan:\localhost\Listener | Where-Object { $_.Keys -contains "Transport=HTTP" } | Remove-Item -Recurse -Force
-Remove-NetFirewallRule -DisplayName "Windows Remote Management (HTTP-In)"
 $certificateforwinrm=New-SelfSignedCertificate -DnsName $fqdn -CertStoreLocation Cert:\LocalMachine\My
 $mycommand = 'winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname="'+$fqdn+'"; CertificateThumbprint="'+$certificateforwinrm.Thumbprint+'"}'
 if (cmd /c $mycommand) {
-  "Created winrm HTTPS listener" | Out-File $logFile -Append
+  datestring -Message "Created winrm HTTPS listener" | Out-File $logFile -Encoding ascii -Append
 }
 Else {
-  "Error creating HTTPS listener. HINT is winrm accessible to local admins? The computer rename script might help" | Out-File $logFile -Append
+  datestring -Message "Error creating HTTPS listener. HINT is winrm accessible to local admins? The computer rename script might help" | Out-File $logFile -Encoding ascii -Append
   Write-Host "ERROR: see "$logFile" for details"
 }
 
-# private network profile
+# remove its firewall rule
+Remove-NetFirewallRule -DisplayName "Windows Remote Management (HTTP-In)"
+
+# allow TCP port 5986 thru firewall.
 set-netconnectionprofile -InterfaceAlias Ethernet -NetworkCategory Private
 New-NetFirewallRule -DisplayName 'WinRM HTTPS' -Profile @('Domain', 'Private') -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('5986')
-
