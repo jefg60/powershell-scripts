bc49a485538881541bc1c2c506638ca4cf4194d4
Jeff Hibberd <jeff@jeffhibberd.com>
2020-07-17 11:32:58 +0100

    parameterize script to enable automated runs

--- win10home/ansible-bootstrap-win10home.ps1
+++ win10home/ansible-bootstrap-win10home.ps1
@@ -18,67 +18,75 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 #
+Param(
+	[string] $ansibleUserName = 'ansible',
+	[string] $ansiblepassword,
+	[Switch] $y = $false
+)
 
 $ErrorActionPreference = "Stop"
 
 function datestring {
-  param($Message)
-  (Get-Date).ToString("yyyy/M/dd hh:mm:ss") + " $Message"
+	param($Message)
+	(Get-Date).ToString("yyyy/M/dd hh:mm:ss") + " $Message"
 }
 
 #Vars
 $logFile = 'C:\log\winrmscript.log'
-$ansibleUserName = 'ansible'
 $fqdn=[System.Net.Dns]::GetHostByName($env:computerName).HostName
-Write-Host "My FQDN appears to be "$fqdn" Press ENTER to continue with this FQDN"
-Write-Host "There is a rename computer script around here somewhere ;)"
-Read-Host
+if ( $y = $false ) {
+	Write-Host "My FQDN appears to be "$fqdn" Press ENTER to continue with this FQDN"
+	Write-Host "There is a rename computer script around here somewhere ;)"
+	Read-Host
+}
 
 #check windows version
 $desiredbuild = 17134
 $detectedbuild = [System.Environment]::OSVersion.Version.Build
 if ( -Not $detectedbuild -ge $desiredbuild) {
-        write-host "This script is for windows build "$desiredbuild" or higher"
-        write-host "It appears to be running on build "$detectedbuild
-        write-host "Please try a different script"
-        Read-Host -Prompt "Press Enter to exit or ctrl-C to return to the shell"
-        Exit
+	write-host "This script is for windows build "$desiredbuild" or higher"
+	write-host "It appears to be running on build "$detectedbuild
+	write-host "Please try a different script"
+	Read-Host -Prompt "Press Enter to exit or ctrl-C to return to the shell"
+	Exit
 } Else {
 Write-host "Build version seems to be in order, here goes..."
 }
 
 #Check if we've been here before
 if (Test-Path $logFile) {
-        write-host "Found a log File at "$logFile" - cowardly refusing to run script twice."
-        write-host "Delete "$logFile" if you want to try again"
-        Read-Host -Prompt "Press Enter to exit or ctrl-C to return to the shell"
-        Exit
+	write-host "Found existing log File at "$logFile
 } Else {
-        Write-Host "no logfile found, continuing"
-        New-Item $logFile -Itemtype file -Force
+	Write-Host "no logfile found, creating it"
+	New-Item $logFile -Itemtype file -Force
 }
 
 #Check that we can adminster winrm (permissions may be wrong)
 try {
-  winrm enumerate winrm/config/Listener
-  datestring -Message "I can enumerate winrm OK" | Out-File $logFile -Encoding ascii -Append
+	winrm enumerate winrm/config/Listener
+	datestring -Message "I can enumerate winrm OK" | Out-File $logFile -Encoding ascii -Append
 }
 Catch {
-  Read-Host -Prompt "Error: Can't enumerate winrm properly. check the rename script for clues"
-  Exit
+	Read-Host -Prompt "Error: Can't enumerate winrm properly. check the rename script for clues"
+	Exit
 }
 
 #ansible user - remove first (silently continue if not there)
 remove-localuser -Name $ansibleUserName -ErrorAction SilentlyContinue
 try {
-  $password = Read-Host -AsSecureString -Prompt "ansible user password:"
-  New-localuser -Name $ansibleUserName -Password $password -ErrorAction Stop
-  datestring -Message "Added ansible user" | Out-File $logFile -Encoding ascii -Append
-  Add-LocalGroupMember -Group Administrators -Member ansible -ErrorAction Stop
-  datestring -Message "Added ansible user to Administrators group" | Out-File $logFile -Encoding ascii -Append
+	if ( $ansiblepassword = $null ) {
+		$ansiblepassword = Read-Host -AsSecureString -Prompt "ansible user password:"
+	}
+	else {
+		$ansiblepassword = $ansiblepassword | ConvertTo-SecureString -AsPlainText -Force
+	}
+	New-localuser -Name $ansibleUserName -Password $password -ErrorAction Stop
+	datestring -Message "Added ansible user" | Out-File $logFile -Encoding ascii -Append
+	Add-LocalGroupMember -Group Administrators -Member ansible -ErrorAction Stop
+	datestring -Message "Added ansible user to Administrators group" | Out-File $logFile -Encoding ascii -Append
 }
 Catch {
-  datestring -Message "Error adding ansible user" | Out-File $logFile -Encoding ascii -Append
+	datestring -Message "Error adding ansible user" | Out-File $logFile -Encoding ascii -Append
 }
 
 # do initial winrm quickconfig (HTTP only)
@@ -89,11 +97,11 @@ Get-ChildItem -Path WSMan:\localhost\Listener | Where-Object { $_.Keys -contains
 $certificateforwinrm=New-SelfSignedCertificate -DnsName $fqdn -CertStoreLocation Cert:\LocalMachine\My
 $mycommand = 'winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname="'+$fqdn+'"; CertificateThumbprint="'+$certificateforwinrm.Thumbprint+'"}'
 if (cmd /c $mycommand) {
-  datestring -Message "Created winrm HTTPS listener" | Out-File $logFile -Encoding ascii -Append
+	datestring -Message "Created winrm HTTPS listener" | Out-File $logFile -Encoding ascii -Append
 }
 Else {
-  datestring -Message "Error creating HTTPS listener. HINT is winrm accessible to local admins? The computer rename script might help" | Out-File $logFile -Encoding ascii -Append
-  Write-Host "ERROR: see "$logFile" for details"
+	datestring -Message "Error creating HTTPS listener. HINT is winrm accessible to local admins? The computer rename script might help" | Out-File $logFile -Encoding ascii -Append
+	Write-Host "ERROR: see "$logFile" for details"
 }
 
 # remove its firewall rule
